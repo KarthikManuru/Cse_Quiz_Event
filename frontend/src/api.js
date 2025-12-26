b6e5e35bce825f55d191a2fd83e07525f4013036
karthikjayavaram <karthik.jayavaram@gmail.com>
2025-12-26 11:25:28 +0530

    updated api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@
 
 // export const API_BASE_URL =
 //   process.env.REACT_APP_API_BASE_URL || DEFAULT_BASE_URL;
-export const API_BASE_URL = "https://cse-quiz-event.onrender.com/api";
+export const API_BASE_URL = "https://cse-webquiz-event.onrender.com";
 // export const API_BASE_URL = "http://localhost:5000/api";
 
 export async function startQuiz(data) {
